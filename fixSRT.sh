e8c1dca4b048925d282fa5b7d923b03552899b1b
Karthik Periagaram <karthik.periagaram@gmail.com>
2014-07-03 21:48:48 -0700

    Initial commit

---
+++ fixSRT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Perform the following operations on an SRT file
+# Convert the file to UTF-8 encoding (and remove any BOM)
+# Renumber the entries
+# Shift SRT timings by specified delay (in seconds)
+# Stretch SRT timings by specified factor (desired/current)
+
+# Requires iconv
+# Requires srttool
+
+exit 0
+
