39bbde015556b5165930e71ce2d6081d4629270b
Karthik Periagaram <karthik.periagaram@gmail.com>
2014-07-03 22:18:37 -0700

    Added a help message
    This help message describes the features desired of this script.

--- fixSRT.sh
+++ fixSRT.sh
@@ -9,5 +9,24 @@
 # Requires iconv
 # Requires srttool
 
+printUsage()
+{
+    cat <<- EOF
+	Usage $PROGNAME [options] FILE.srt
+
+	This script will clean up SRT files by converting the encoding to UTF-8, removing any BOM (byte-order marks) left in the resulting file.
+	The entries in the SRT file will also be renumbered in sequential order.
+
+	Optionally, this script can also be used to adjust the delay and fps timings of the SRT file.
+
+	The input file will be moved to FILE.old and the cleaned up file will be written in its place.
+
+	OPTIONS
+	-d --delay      Shift all entries by this delay [in milliseconds]
+	-s --stretch    Stretch the times by this factor
+	-h --help       Print this help message
+	EOF
+}
+
 exit 0
 
